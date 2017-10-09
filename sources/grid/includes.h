bee9feafe2e18b1febd6ac97ee93e780495b94cf
Tomas Maly <malytomas@ucpu.cz>
2017-10-09 20:11:41 +0200

    moved asset manager from client to core

--- sources/grid/includes.h
+++ sources/grid/includes.h
@@ -5,17 +5,18 @@
 #include <cage-core/filesystem.h>
 #include <cage-core/entities.h>
 #include <cage-core/config.h>
+#include <cage-core/assets.h>
 #include <cage-core/utility/timer.h>
 #include <cage-core/utility/hashString.h>
 #include <cage-core/utility/ini.h>
 #include <cage-core/utility/color.h>
 #include <cage-core/utility/spatial.h>
+#include <cage-core/utility/textPack.h>
 
 #include <cage-client/core.h>
 #include <cage-client/graphic.h>
 #include <cage-client/sound.h>
 #include <cage-client/assets.h>
-#include <cage-client/utility/textPack.h>
 #include <cage-client/gui.h>
 #include <cage-client/engine.h>
 
